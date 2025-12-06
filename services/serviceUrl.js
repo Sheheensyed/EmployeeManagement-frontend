771eadf8bd7abc4ce77048e8de61e8718c86a897
Sheheensyed <sheheensyed0@gmail.com>
2025-12-06 12:50:29 +0530

    updated server url

--- services/serviceUrl.js
+++ services/serviceUrl.js
@@ -1,1 +1,2 @@
-export const serverUrl='https://employeemanagement-server-k84l.onrender.com'
+// export const serverUrl='https://employeemanagement-server-k84l.onrender.com'
+export const serverUrl='https://employeemanagement-server-6.onrender.com'
